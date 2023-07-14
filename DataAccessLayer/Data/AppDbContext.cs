87829f6da69edbd5e69141f4e5cc54890befffbd
Khush <khush@simformsolutions.com>
2023-07-14 13:27:00 +0530

    Practical 22 added

---
+++ DataAccessLayer/Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿
+using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {            
+        }        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connectionString = @"Data Source=SF-CPU-332\SQLEXPRESS;Initial Catalog=Practical22;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().Property<DateTime>("CreatedDate");
+            modelBuilder.Entity<Employee>().Property<DateTime>("UpdatedDate");
+        }
+        public DbSet<Employee> Employees { get; set;}
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ DataAccessLayer/Models/EmployeeDto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLayer.Models
+{
+    public class EmployeeDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal Salary { get; set; }
+        [Required]
+        public int DepartmentId { get; set; }
+    }
+}
